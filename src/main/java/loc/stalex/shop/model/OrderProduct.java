5b69b385d6ff70557d569854a56fb4cf17465fa8
StepchUk <stepchyk@gmail.com>
2020-01-09 13:43:17 +0200

    Created OrderProduct model

---
+++ src/main/java/loc/stalex/shop/model/OrderProduct.java
@@ -0,0 +1,61 @@
+package loc.stalex.shop.model;
+
+public class OrderProduct {
+    
+    private int id;
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private double price;
+
+    public OrderProduct() {
+    }
+
+    public OrderProduct(int id, int orderId, int productId, int quantity, double price) {
+        this.id = id;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return this.orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
