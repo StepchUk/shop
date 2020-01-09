9ce8d79e5ed0748a84f85f27ebeac8a009523290
StepchUk <stepchyk@gmail.com>
2020-01-09 13:36:35 +0200

    Created Cart model

---
+++ src/main/java/loc/stalex/shop/model/Cart.java
@@ -0,0 +1,51 @@
+package loc.stalex.shop.model;
+
+public class Cart {
+    
+    private int id;
+    private int userId;
+    private int productId;
+    private int quantity;
+
+    public Cart() {
+    }
+
+    public Cart(int id, int userId, int productId, int quantity) {
+        this.id = id;
+        this.userId = userId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return this.productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
