54b910770394528539b8e18f59b9ed38a4715303
StepchUk <stepchyk@gmail.com>
2020-01-09 13:39:50 +0200

    Created Manufacturer model

---
+++ src/main/java/loc/stalex/shop/model/Manufacturer.java
@@ -0,0 +1,41 @@
+package loc.stalex.shop.model;
+
+public class Manufacturer {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Manufacturer() {
+    }
+
+    public Manufacturer(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
